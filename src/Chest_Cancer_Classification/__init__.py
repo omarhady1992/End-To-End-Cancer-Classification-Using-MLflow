ba0966d27846e518439388825a98070b3c52ec69
Omar_Hady <ohady1992@gmail.com>
2024-11-13 16:16:41 +0200

    "Bug Fix: add func to make directory"

--- src/Chest_Cancer_Classification/__init__.py
+++ src/Chest_Cancer_Classification/__init__.py
@@ -6,6 +6,7 @@ logging_format = "[%(asctime)s - %(levelname)s - %(module)s - %(message)s]"
 
 log_dir = "logs"
 log_fp = os.path.join(log_dir, "running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
     level= logging.INFO,
