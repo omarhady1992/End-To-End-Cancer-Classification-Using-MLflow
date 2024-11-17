f0aab412613190f5b9aa9c17ed6bbf31dbba983a
Omar_Hady <ohady1992@gmail.com>
2024-11-18 01:08:49 +0200

    Added 1st stage (data ingestion) to pipeline

---
+++ main.py
@@ -0,0 +1,16 @@
+from Chest_Cancer_Classification import logger
+from Chest_Cancer_Classification.pipeline.first_stage_data_ingestion import DataIngestionTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/Chest_Cancer_Classification/pipeline/first_stage_data_ingestion.py
@@ -0,0 +1,31 @@
+from Chest_Cancer_Classification.config.configuration import ConfigurationManager
+from Chest_Cancer_Classification.components.data_ingestion import DataIngest
+from Chest_Cancer_Classification import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        configuration = ConfigurationManager()
+        data_config = configuration.get_data_ingestion()
+        data_ingestion = DataIngest(config=data_config)
+        data_ingestion.download_data_googledrive()
+        data_ingestion.unzip_data()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
