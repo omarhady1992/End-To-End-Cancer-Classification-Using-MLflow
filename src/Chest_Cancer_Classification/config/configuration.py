77958b2bea9d9854e31a0c589bed40a2dbcc25a6
Omar_Hady <ohady1992@gmail.com>
2024-11-19 02:19:07 +0200

    Added base model configurations

--- src/Chest_Cancer_Classification/config/configuration.py
+++ src/Chest_Cancer_Classification/config/configuration.py
@@ -1,6 +1,6 @@
 from Chest_Cancer_Classification.constants import *
 from Chest_Cancer_Classification.utils.common import create_directories, read_yaml
-from Chest_Cancer_Classification.entity.config_entity import DataIngestionConfiguration
+from Chest_Cancer_Classification.entity.config_entity import DataIngestionConfiguration, BaseModelConfig
 
 
 class ConfigurationManager:
@@ -29,3 +29,20 @@ class ConfigurationManager:
         )
         
         return data_ingestion_config
+    
+
+    def get_base_model(self)-> BaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        base_model_config = BaseModelConfig(
+            root_dir = config.root_dir,
+            base_model_path = config.base_model_path,
+            updated_base_model_path = config.updated_base_model_path,
+            params_image_size = self.params.IMAGE_SIZE,
+            params_learning_rate= self.params.LEARNING_RATE,
+            params_include_top= self.params.INCLUDE_TOP,
+            params_weight= self.params.WEIGHTS,
+            params_classes= self.params.CLASSES)
+        return base_model_config
