fe88cdb911d8bedc39bcfcd4a7ebadccd83aa3d6
Omar_Hady <ohady1992@gmail.com>
2024-12-03 23:17:11 +0200

    Added Evaluation to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.Chest_Cancer_Classification import logger
 from src.Chest_Cancer_Classification.pipeline.first_stage_data_ingestion import DataIngestionTrainingPipeline
 from src.Chest_Cancer_Classification.pipeline.second_stage_prepare_model import PrepareBaseModelTrainingPipeline
 from src.Chest_Cancer_Classification.pipeline.third_stage_training import TrainingPipeline
+from src.Chest_Cancer_Classification.pipeline.fourth_stage_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -38,3 +39,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
